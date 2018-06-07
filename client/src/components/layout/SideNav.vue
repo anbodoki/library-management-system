7e7f2b29f2ab8ba406774c1beebaf68abac06c1b
ninucabas <nbasi13@freeuni.edu.ge>
2018-06-07 10:40:50 +0400

    add side nav bar elements

--- client/src/components/layout/SideNav.vue
+++ client/src/components/layout/SideNav.vue
@@ -2,9 +2,33 @@
     <nav class="side-nav" role="navigation">
         <span class="side-nav-title text-primary-light f-nino-mtavruli">ნავიგაცია</span>
         <ul class="side-nav-menu">
-            <li class="side-nav-menu-item">
-                <img src=""/>
+            <li class="side-nav-menu-item flex-center-vertically">
+                <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
                 <span>მთავარი</span>
+            </li>
+            <li class="side-nav-menu-item flex-center-vertically">
+                <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
+                <span>სილაბუსები</span>
+            </li>
+            <li class="side-nav-menu-item flex-center-vertically">
+                <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
+                <span>კატეგორიები</span>
+            </li>
+        </ul>
+        <div class="divider horizontal"></div>
+        <span class="side-nav-title text-primary-light f-nino-mtavruli">ჩემი წიგნები</span>
+        <ul class="side-nav-menu">
+            <li class="side-nav-menu-item flex-center-vertically">
+                <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
+                <span>გატანილი</span>
+            </li>
+            <li class="side-nav-menu-item flex-center-vertically">
+                <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
+                <span>ფავორიტები</span>
+            </li>
+            <li class="side-nav-menu-item flex-center-vertically">
+                <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
+                <span>ისტორია</span>
             </li>
         </ul>
     </nav>
--- client/src/router.js
+++ client/src/router.js
@@ -9,18 +9,18 @@ const NotFound = () => import(/* webpackChunkName: "not-found" */ './components/
 const routes = [
     {
         path: '/', component: require('./components/layouts/defaultLayout.vue'), children: [
-            { path: '/', name: 'home', redirect: '/home' },
-            { path: '/home', name: 'home', component: Home, meta: { title: 'Home' } },
+            {path: '/', name: 'home', redirect: '/home'},
+            {path: '/home', name: 'home', component: Home, meta: {title: 'Home'}},
         ]
     },
-  { path: '*', component: NotFound, meta: { title: 'Not Found' } },
+    {path: '*', component: NotFound, meta: {title: 'Not Found'}},
 ];
 
-const router = new VueRouter({ routes, mode: 'history' });
+const router = new VueRouter({routes, mode: 'history'});
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title;
-  next();
+    document.title = to.meta.title;
+    next();
 });
 
 export default router;
