b26d66ab56e6065008befd6150a9681aac54eef0
ninucabas <nbasi13@freeuni.edu.ge>
2018-06-09 22:26:06 +0400

    side nav bar hovers

--- client/src/components/layout/SideNav.vue
+++ client/src/components/layout/SideNav.vue
@@ -2,15 +2,15 @@
     <nav class="side-nav" role="navigation">
         <span class="side-nav-title text-primary-light f-nino-mtavruli">ნავიგაცია</span>
         <ul class="side-nav-menu">
-            <li class="side-nav-menu-item flex-center-vertically">
-                <img class="side-nav-menu-item-icon" src="../../images/icons/cargoType.svg"/>
+            <li class="side-nav-menu-item flex-center-vertically btn">
+                <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
                 <span>მთავარი</span>
             </li>
-            <li class="side-nav-menu-item flex-center-vertically">
+            <li class="side-nav-menu-item flex-center-vertically btn">
                 <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
                 <span>სილაბუსები</span>
             </li>
-            <li class="side-nav-menu-item flex-center-vertically">
+            <li class="side-nav-menu-item flex-center-vertically btn">
                 <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
                 <span>კატეგორიები</span>
             </li>
@@ -18,15 +18,15 @@
         <div class="divider horizontal"></div>
         <span class="side-nav-title text-primary-light f-nino-mtavruli">ჩემი წიგნები</span>
         <ul class="side-nav-menu">
-            <li class="side-nav-menu-item flex-center-vertically">
+            <li class="side-nav-menu-item flex-center-vertically btn">
                 <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
                 <span>გატანილი</span>
             </li>
-            <li class="side-nav-menu-item flex-center-vertically">
+            <li class="side-nav-menu-item flex-center-vertically btn">
                 <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
                 <span>ფავორიტები</span>
             </li>
-            <li class="side-nav-menu-item flex-center-vertically">
+            <li class="side-nav-menu-item flex-center-vertically btn">
                 <img class="side-nav-menu-item-icon" src="/dist/imgs/icons/cargoType.svg"/>
                 <span>ისტორია</span>
             </li>
