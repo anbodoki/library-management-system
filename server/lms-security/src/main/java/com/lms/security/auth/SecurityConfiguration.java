73987196eb93f0c404e65a7e1c61984ee2b9c2df
ananobodokia <anano.bodokia@gmail.com>
2018-06-30 19:06:48 +0400

    Update sercurity configuration for clients

--- server/lms-security/src/main/java/com/lms/security/auth/SecurityConfiguration.java
+++ server/lms-security/src/main/java/com/lms/security/auth/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.lms.security.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -14,10 +21,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailsService userDetailsService;
+    private final ServletContext servletContext;
 
     @Autowired
-    public SecurityConfiguration(CustomUserDetailsService userDetailsService) {
+    public SecurityConfiguration(CustomUserDetailsService userDetailsService, ServletContext servletContext) {
         this.userDetailsService = userDetailsService;
+        this.servletContext = servletContext;
     }
 
     @Override
@@ -38,9 +47,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .httpBasic()
+                .and().exceptionHandling()
+                .authenticationEntryPoint(unauthorizedEntryPoint())
                 .and()
                 .csrf().disable()
                 .logout()
                 .logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public AuthenticationEntryPoint unauthorizedEntryPoint() {
+        return (HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) -> {
+            String url = request.getRequestURI();
+            if (url.startsWith(servletContext.getContextPath() + "/client/client-api/") || url.startsWith(servletContext.getContextPath() + "//client/client-api/")) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            } else {
+                response.sendRedirect(servletContext.getContextPath() + "/login");
+            }
+        };
     }
 }
