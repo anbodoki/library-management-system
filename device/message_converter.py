fd0104bdae7912b1fde749b1ca9521afbe80d266
ananobodo <anano.bodokia.bodo@gmail.com>
2018-07-14 20:13:19 +0400

    Initial device client commit

---
+++ device/message_converter.py
@@ -0,0 +1,8 @@
+
+
+def generate_getBookInfo_message(self, book_id):
+    pass
+
+
+def parse_getBookInfo_response(response):
+    pass
---
+++ device/tcp_client.py
@@ -0,0 +1,3 @@
+
+def send_message(request_message):
+    pass
