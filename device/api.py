8aadbea3402c6728716692edd031499bd06ec65a
ananobodo <anano.bodokia.bodo@gmail.com>
2018-07-14 20:13:19 +0400

    Initial device client commit

---
+++ device/api.py
@@ -0,0 +1,17 @@
+from message_converter import generate_getBookInfo_message, parse_getBookInfo_response
+from tcp_client import send_message
+
+
+def get_book_info(book_id):
+    request_message = generate_getBookInfo_message(book_id)
+    response = send_message(request_message)
+    book_info = parse_getBookInfo_response(response)
+    return book_info
+
+
+def get_user_info(user_id):
+    pass
+
+
+def submit_book_request(user_id, book_id):
+    pass
