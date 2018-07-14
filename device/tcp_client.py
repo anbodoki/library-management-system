93610dd9dcb2b8fc05887778f2644b1683284af0
ninucabas <nbasi13@freeuni.edu.ge>
2018-07-14 23:03:42 +0400

    Server connection established

--- device/tcp_client.py
+++ device/tcp_client.py
@@ -1,3 +1,16 @@
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
 
 def send_message(request_message):
-    return '*b&o&date&book name by author&crc16'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(request_message)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+    # return '*b&o&date&book name by author&crc16'
+    return data
