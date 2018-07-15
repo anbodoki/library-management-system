7a8d2fbd8ad52546ff14b00d7dcc35c90b1eabb3
xatiamacharadze <kmach12@freeuni.edu.ge>
2018-07-15 17:16:32 +0400

    Add message validations

--- device/server.py
+++ device/server.py
@@ -2,20 +2,29 @@ import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 
-
-conn, addr = s.accept()
-print('Connection address:', addr)
+# conn, addr = s.accept()
+# print('Connection address:', addr)
 
 while True:
+    conn, addr = s.accept()
+    print('Connection address:', addr)
     data = conn.recv(BUFFER_SIZE)
     if not data:
         break
     print("received data:", data)
-    conn.send(b'*b&o&date&book name by author&crc16')
-    conn.close()
+
+    data = data.decode()
+
+    msg_type, msg_status, date, msg_data, crc16 = data[1:].split('&')
+
+    if msg_type == 'b':
+        conn.send(b'*b&o&date&book name by author&crc16')
+        print("send data:", b'*b&o&date&book name by author&crc16')
+
+    # conn.close()
