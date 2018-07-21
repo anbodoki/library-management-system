e319cbc14ad64c3758d7609889c8f61e68517dc8
xatia <kmach12@freeuni.edu.ge>
2018-07-21 18:47:54 +0400

    Device client Initial commit

---
+++ device/client/src/js/components/Keyboard.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Keyboard"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ device/client/webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FileWatcherPlugin = require('file-watcher-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'src/images', to: 'images' },
+            //{from: 'src/fonts', to:  'fonts'}
+        ]),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                ]
+            },
+
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+
+            {
+                test: /\.(html|html)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+
+            },
+
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                }
+            },
+
+        ],
+    },
+
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    devtool: '#eval-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins = (module.exports.plugins).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
