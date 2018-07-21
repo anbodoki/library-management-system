95273af9e412fb00ddb3a4319068dfe63cd304e7
xatia <kmach12@freeuni.edu.ge>
2018-07-22 00:08:46 +0400

    Fix cors

--- device/server/application/app.py
+++ device/server/application/app.py
@@ -1,24 +1,25 @@
 import sys
 
-from application.api.book_submission.book_submission_api import BookSubmissions
 
 sys.path.append('./..')
 
+from application.api.book_submission.book_submission_api import BookSubmissions
 from application.api.client.client_api import ClientInfo
 from application.api.book.book_api import BookInfo
 
-from application.extensions import api
+from application.extensions import api, cors
 from application.flask_app import app
 
 
 def init_extensions():
     api.init_app(app)
+    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
 
 
 def init_resources():
-    api.add_resource(BookInfo, '/books/<string:book_id>')
-    api.add_resource(ClientInfo, '/clients/<string:client_id>')
-    api.add_resource(BookSubmissions, '/booksubmissions')
+    api.add_resource(BookInfo, '/api/books/<string:book_id>')
+    api.add_resource(ClientInfo, '/api/clients/<string:client_id>')
+    api.add_resource(BookSubmissions, '/api/booksubmissions')
 
 
 def init_app():
@@ -29,4 +30,4 @@ def init_app():
 
 
 if __name__ == '__main__':
-    init_app().run(port=8080)
+    init_app().run(port=8082)
