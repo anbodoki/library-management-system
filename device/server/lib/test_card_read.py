b2bbd0ed23cd0d4276b60852e573ebf44f2c8ee5
xatia <kmach12@freeuni.edu.ge>
2018-07-23 02:03:27 +0400

    Bugfix

--- device/server/lib/test_card_read.py
+++ device/server/lib/test_card_read.py
@@ -1,14 +1,10 @@
 import io
 import subprocess
-from random import random
-from time import sleep
 
 
 def read_card(callback):
-    # sleep(5)
-    callback('123456789')
-    # reader = subprocess.Popen(['java', '-cp', '../lib/RXTXcomm.jar:../lib/reader.jar', 'com.reader.Main'], stdout=subprocess.PIPE)
-    # lines = io.TextIOWrapper(reader.stdout, encoding='utf-8')
-    # for line in lines:
-    #     if line.startswith("#"):
-    #         callback(line[1:])
+    reader = subprocess.Popen(['java', '-cp', '../lib/RXTXcomm.jar:../lib/reader.jar', 'com.reader.Main'], stdout=subprocess.PIPE)
+    lines = io.TextIOWrapper(reader.stdout, encoding='utf-8')
+    for line in lines:
+        if line.startswith("#"):
+            callback(line[1:].rstrip())
