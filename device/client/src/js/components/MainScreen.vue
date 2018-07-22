268cb32d06e7e898d588906a7b7be91dcd8c91d7
ninucabas <nbasi13@freeuni.edu.ge>
2018-07-22 20:32:52 +0400

    Extract keyboard component

---
+++ device/client/src/js/components/MainScreen.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="wrapper">
+        <div class="phone">
+            <div class="phone-container">
+                <div class="left-container">
+                    <input type="text" maxlength="11" v-model="bookId"
+                           class="number-input" id="numberInput" value=""
+                           placeholder="Enter Book ID"/>
+
+                    <button class="submit-btn" type="submit" @click.prevent="submit()">Submit</button>
+                </div>
+
+                <keyboard v-on:number-click="numberClick" v-on:delete-number="deleteNumber"></keyboard>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {BooksService} from "../resource";
+    import Keyboard from "./Keyboard";
+
+    export default {
+        name: "MainScreen",
+        data() {
+            return {
+                bookId: ""
+            }
+        },
+
+        methods: {
+            numberClick(number) {
+                this.bookId += number;
+            },
+            deleteNumber() {
+                this.bookId = this.bookId.slice(0, -1);
+            },
+            submit() {
+                BooksService.getBookInfo(this.bookId).then(res => {
+                    console.log('keyboard success', res);
+                    if (res === 'Book not exits') {
+                        this.$emit('error', res, 'error');
+                    } else {
+                        this.$emit('book-info-updated', res);
+                    }
+                }).catch(err => {
+                    console.log('keyboard error', err);
+                    this.$emit('error', err, 'error');
+                })
+            }
+        },
+        components: {
+            Keyboard
+        }
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
